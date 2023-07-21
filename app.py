c97a0cf67656f5eb3fbf82e40d49673c9831df2e
Dipen hore <83294662+Dipenhore8200@users.noreply.github.com>
2023-07-21 21:02:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,15 +17,50 @@ def main():
     # Initialize empty list to store user inputs
     user_inputs = []
 
+    gender_options = {
+        "Female": 0,
+        "Male": 1
+    }
+
+    # Dropdown for gender selection
+    selected_gender = st.selectbox("Select Gender", list(gender_options.keys()))
+
+    # Get the corresponding value based on the selected label
+    gender_value = gender_options[selected_gender]
+    user_inputs.append( gender_value)
+
+    dependency_options = {
+        "1": 1,
+        "2": 2,
+        "3 or more": 3
+    }
+
+    # Dropdown for family size selection
+    selected_dependency = st.selectbox("Select dependeny", list(dependency_options.keys()))
+    
+    # Get the corresponding value based on the selected label
+    dependency_value =dependency_options[selected_dependency]
+    user_inputs.append( dependency_value)
     # Dropdowns for binary options (Yes or No)
-    questions_binary = ["Question 1", "Question 2", "Question 3", "Question 4", "Question 5"]
+    
+    questions_binary = ["Select Marital status", "select employedment status", "Property_Area"]
     for question in questions_binary:
         user_input = st.selectbox(f"{question} (Select Yes or No)", ("Yes", "No"))
         binary_input = yes_no_to_binary(user_input)
         user_inputs.append(binary_input)
+    
+    education_op = {
+        "graduate": 1,
+        "Not graduate": 0
+    }
+    selected_education = st.selectbox("Select educational status", list(education_op.keys()))
+
+    # Get the corresponding value based on the selected label
+    education_value = education_op[selected_education]
+    user_inputs.append(education_value)
 
     # Inputs for integer numbers
-    questions_integer = ["Question 6", "Question 7", "Question 8", "Question 9", "Question 10", "Question 11"]
+    questions_integer = ["Enter ApplicantIncome", "Enter CoapplicantIncome", "Enter LoanAmount", "Enter Loan_Amount_Term", "Enter Credit_History	", "Property_Area"]
     for i in range(6):
         question = questions_integer[i]
         question_input = st.text_input(f"{question} (Integer Number)", value="0")
